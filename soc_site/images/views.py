157fadd99b9059eed47a2d505e71fcec92f6f6af
Shvec Sergei <fejanin@mail.ru>
2024-11-03 17:15:46 +0300

    Display images.

--- soc_site/images/views.py
+++ soc_site/images/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import ImageCreatedForm
 from .models import Image
 
@@ -58,3 +59,38 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
         return JsonResponse({'status': 'error'})
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    print(images)
+    paginator = Paginator(images, 6)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    print(images_only)
+    try:
+        images = paginator.page(page)
+        print('YES')
+    except PageNotAnInteger:
+        print('PageNotAnInteger')
+        # если страница не является числом,
+        # то вернуть первую страницу
+        images = paginator.page(1)
+    except EmptyPage:
+        print('EmptyPage')
+        if images_only:
+            # если AJAX-запрос и страница вне диапазона,
+            # то вернуть пустую страницу
+            return HttpResponse('')
+        # если страница вне диапазона,
+        # то вернуть последнюю страницу результатов
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request,
+                      'images/image/list_images.html',
+                      {'section': 'images',
+                       'images': images})
+    return render(request,
+                  'images/image/list.html',
+                  {'section': 'images',
+                   'images': images})
